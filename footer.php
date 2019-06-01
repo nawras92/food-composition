8735f97e6eafe19b64a37755b839b84c0a81135a
Nawras Ali <nawras@learnwithnaw.net>
2019-06-02 02:27:41 +0300

    fix nav overlapping the content

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-
+<div style="height:100px;"> </div>
     <nav class="navbar fixed-bottom navbar-expand-sm navbar-dark bg-dark">
         <a class="navbar-brand" href="">Food Composition</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
@@ -7,7 +7,7 @@
         <div class="collapse navbar-collapse" id="navbarCollapse">
          <ul class="navbar-nav mr-auto">
            <li class="nav-item">
-            <a class="nav-link" href="">Search</a>
+            <a class="nav-link" href="index.php">Search</a>
            </li>
          </ul>
          </div>
