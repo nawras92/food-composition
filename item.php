de17e0939a6de56e307712b3039f7c1cc3251d76
Nawras Ali <nawras@learnwithnaw.net>
2019-06-02 01:06:34 +0300

    display nutrients in a table

--- item.php
+++ item.php
@@ -14,8 +14,35 @@
      curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
      $result = curl_exec($curl);
      curl_close($curl);
-     echo $result;
+     $json = json_decode($result);
    ?>
+
+   <div class="row">
+    <div class="col-md-12">
+      <table class="table table-striped table-responsive">
+       <thead>
+         <tr>
+           <th>Name</th>
+           <th>Value</th>
+           <th>Unit</th>
+           <th>Group</th>
+         </tr>
+       </thead>
+       <tbody>
+         <?php foreach($json->report->food->nutrients as $nutrient): ?>
+           <tr>
+            <td><?php echo $nutrient->name;?></td>
+            <td><?php echo $nutrient->value;?></td>
+            <td><?php echo $nutrient->unit;?></td>
+            <td><?php echo $nutrient->group;?> </td>
+           </tr>
+         <?php endforeach; //end foreach($json->report->food->nutrients as $nutrient): ?>
+       </tbody>
+      </table>
+    </div>
+   </div>
+   </div>
+
 
 
 </div>
