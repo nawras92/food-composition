0b2f45348c5809796d1e0767b8bf9e9e88788736
Nawras Ali <nawras@learnwithnaw.net>
2019-06-01 23:33:29 +0300

    create item page

---
+++ item.php
@@ -0,0 +1,20 @@
+<?php include_once('header.php'); ?>
+
+<div class="container">
+  <div class="jumbotron mt-3 text-center">
+   <h1>Food Composition for butter ------</h1>
+   <p class="lead">Here you are the food compositions of what you searched for</p>
+  </div>
+
+
+
+
+</div>
+
+
+
+
+
+
+
+<?php include_once('footer.php'); ?>
